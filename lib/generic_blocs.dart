57197a7ce4014124fef11e33ea9dc2b3971c9665
Marcos Theophilo Gobbi Adamczuk <marcos.adamczuk@gmail.com>
2020-02-20 13:54:42 -0300

    refactored

--- lib/generic_blocs.dart
+++ lib/generic_blocs.dart
@@ -1,5 +1,5 @@
 library generic_blocs;
 
-export './src/infinite_list/infinite_list_bloc.dart';
-export './src/infinite_list/infinite_list_event.dart';
-export './src/infinite_list/infinite_list_state.dart';
+export 'src/infinite_list/bloc/infinite_list_bloc.dart';
+export 'src/infinite_list/bloc/infinite_list_event.dart';
+export 'src/infinite_list/bloc/infinite_list_state.dart';
