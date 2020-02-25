3b418f734c8028051142c2592d7424c56d6713c3
Marcos Theophilo Gobbi Adamczuk <marcos.adamczuk@gmail.com>
2020-02-25 10:41:36 -0300

    updated generic_blocs.dart

--- lib/generic_blocs.dart
+++ lib/generic_blocs.dart
@@ -1,14 +1,14 @@
 library generic_blocs;
 
-export 'src/infinite_list/bloc/infinite_list_bloc.dart';
-export 'src/infinite_list/bloc/infinite_list_event.dart';
-export 'src/infinite_list/bloc/infinite_list_state.dart';
-export 'src/infinite_list/widgets/infinite_list.dart';
-
+export 'src/double_filter/bloc/double_filter_bloc.dart';
+export 'src/double_filter/bloc/double_filter_event.dart';
+export 'src/filter/bloc/filter_bloc.dart';
+export 'src/filter/bloc/filter_event.dart';
 export 'src/finite_list/bloc/finite_list_bloc.dart';
 export 'src/finite_list/bloc/finite_list_event.dart';
 export 'src/finite_list/bloc/finite_list_state.dart';
 export 'src/finite_list/widgets/finite_list.dart';
-
-export 'src/filter/bloc/filter_bloc.dart';
-export 'src/filter/bloc/filter_event.dart';
+export 'src/infinite_list/bloc/infinite_list_bloc.dart';
+export 'src/infinite_list/bloc/infinite_list_event.dart';
+export 'src/infinite_list/bloc/infinite_list_state.dart';
+export 'src/infinite_list/widgets/infinite_list.dart';
